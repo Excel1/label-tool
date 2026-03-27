19b6d84aa79a8f115cdac3f84a4425b855d18b1d
JK <support@jk-powered.de>
2026-03-27 12:59:10 +0100

    Add

--- src/env.d.ts
+++ src/env.d.ts
@@ -5,3 +5,24 @@ declare namespace NodeJS {
     VUE_ROUTER_BASE: string | undefined;
   }
 }
+
+interface ImportMetaEnv {
+  readonly VITE_APP_TRANSPORT_MODE: 'mock' | 'mqtt';
+  readonly VITE_MQTT_BROKER_URL: string;
+  readonly VITE_MQTT_USERNAME?: string;
+  readonly VITE_MQTT_PASSWORD?: string;
+  readonly VITE_MQTT_CLIENT_ID_PREFIX: string;
+  readonly VITE_MQTT_INPUT_TOPIC: string;
+  readonly VITE_MQTT_OUTPUT_TOPIC: string;
+  readonly VITE_MOCK_INCLUDE_LABELS: 'true' | 'false';
+  readonly VITE_YOLO_LABEL_CLASSES?: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
+declare module '*.txt?raw' {
+  const content: string;
+  export default content;
+}
